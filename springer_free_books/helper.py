7d856b8998249fa98c77459c961f18811e7e2448
장정호 <cookyman@wizzpay.co.kr>
2020-05-04 15:02:17 +0900

    springer_free_books 검색 및 다운로더

---
+++ springer_free_books/helper.py
@@ -0,0 +1,130 @@
+import os
+import re
+import requests
+import shutil
+import time
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+BOOK_TITLE = 'Book Title'
+CATEGORY   = 'English Package Name'
+MAX_FILENAME_LEN = 145
+
+def create_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+def create_book_file(base_path, bookname, patch):
+    """
+    Create a file to store book content and return the file reference
+    if it is newly created. Otherwise return None.
+    """
+    output_file = os.path.join(base_path, bookname + patch['ext'])
+    if os.path.exists(output_file):
+        return None
+    return output_file
+
+
+def print_invalid_categories(invalid_categories):
+    if len(invalid_categories) > 0:
+        series = pd.Series(invalid_categories)
+        # Remove duplicates
+        invalid_categories = series[~series.duplicated()]
+        s = 'categories' if len(invalid_categories) > 1 else 'category'
+        print("The following invalid book {} will be ignored:".format(s))
+        for i, name in enumerate(invalid_categories):
+            print(" {}. {}".format((i + 1), name))
+        print('')
+
+
+def print_summary(books, invalid_categories, args):
+    # Set Pandas to no maximum row limit
+    pd.set_option('display.max_rows', None)
+    if args.verbose:
+        # Print all titles to be downloaded
+        print(books.loc[:, (BOOK_TITLE, CATEGORY)])
+    print("\n{} titles ready to be downloaded...".format(len(books.index)))
+    print_invalid_categories(invalid_categories)
+
+
+def filter_books(books, indices):
+    if len(indices) == 0:
+        # If no books selected, then select all books
+        indices = range(0, len(books.index))
+    # Return the filtered books
+    return books.loc[indices, :]
+
+
+def indices_of_categories(categories, books):
+    invalid_categories = []
+    t = pd.Series(np.zeros(len(books.index), dtype=bool))
+    for c in categories:
+        tick_list = books[CATEGORY].str.contains(
+            '^' + c + '$', flags=re.IGNORECASE, regex=True
+        )
+        if tick_list.any():
+            t = t | tick_list
+        else:
+            invalid_categories.append(c)
+    return books.index[t].tolist(), invalid_categories
+
+
+def download_book(url, book_path):
+    if not os.path.exists(book_path):
+        with requests.get(url, stream=True) as req:
+            path = create_path('./tmp')
+            tmp_file = os.path.join(path, '_-_temp_file_-_.bak')
+            with open(tmp_file, 'wb') as out_file:
+                shutil.copyfileobj(req.raw, out_file)
+                out_file.close()
+            shutil.move(tmp_file, book_path)
+
+
+def download_book_if_exists(request, output_file, patch):
+    new_url = request.url.replace('%2F','/').replace('/book/', patch['url']) + patch['ext']
+    print(new_url)
+    request = requests.get(new_url, stream=True)
+    if request.status_code == 200:
+        download_book(new_url, output_file)
+
+
+def download_books(books, folder, patches):
+    for title, author, edition, url, isbn, category in books:
+        dest_folder = create_path(os.path.join(folder, category))
+        bookname = compose_bookname(title, author, edition, isbn)
+        request = None
+        for patch in patches:
+            try:
+                output_file = create_book_file(dest_folder, bookname, patch)
+                if output_file is not None:
+                    request = requests.get(url) if request is None else request
+                    download_book_if_exists(request, output_file, patch)
+            except (OSError, IOError) as e:
+                print(e)
+                title = title.encode('ascii', 'ignore').decode('ascii')
+                print('* Problem downloading: {}, so skipping it.'
+                        .format(title))
+                time.sleep(30)
+                request = None                    # Enforce new get request
+                # then continue to download the next book
+            print(f"{title} 다운로드 완료")
+
+
+replacements = {'/':'-', '\\':'-', ':':'-', '*':'', '>':'', '<':'', '?':'', \
+                '|':'', '"':''}
+
+def compose_bookname(title, author, edition, isbn):
+    bookname = title + ' - ' + author + ', ' + edition + ' - ' + isbn
+    if(len(bookname) > MAX_FILENAME_LEN):
+        bookname = title + ' - ' + author.split(',')[0] + ' et al., ' + \
+                    edition + ' - ' + isbn
+    if(len(bookname) > MAX_FILENAME_LEN):
+        bookname = title + ' - ' + author.split(',')[0] + ' et al. - ' + isbn
+    if(len(bookname) > MAX_FILENAME_LEN):
+        bookname = title + ' - ' + isbn
+    if(len(bookname) > MAX_FILENAME_LEN):
+        bookname = title[:(MAX_FILENAME_LEN - 20)] + ' - ' + isbn
+    bookname = bookname.encode('ascii', 'ignore').decode('ascii')
+    return "".join([replacements.get(c, c) for c in bookname])
---
+++ springer_free_books/spring_free_books.py
@@ -0,0 +1,155 @@
+import sqlite3
+import os
+import numpy
+import argparse
+import pandas as pd
+import requests
+import shutil
+from helper import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--folder', help='folder to store downloads')
+
+parser.add_argument(
+    '-i','--init', action='store_true', help='init database'
+)
+
+parser.add_argument(
+    '-v','--verbose', action='store_true', help='show more details'
+)
+
+args = parser.parse_args()
+folder = create_path(args.folder if args.folder else '.\downloads')
+
+if args.init == True:
+    os.remove('freebook.db')
+
+conn = sqlite3.connect('freebook.db')
+cs = conn.cursor()
+
+table_url = 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4'
+table = 'table_' + table_url.split('/')[-1] + '.xlsx'
+table_path = os.path.join('./', table)
+
+if not os.path.exists(table_path):
+    books = pd.read_excel(table_url)
+    # Save table in the download folder
+    books.to_excel(table_path)
+else:
+    books = pd.read_excel(table_path, index_col=0, header=0)
+
+header = books.columns.tolist()
+contents = books.values.tolist()
+
+query = f''' CREATE TABLE IF NOT EXISTS "FREE_BOOKS" (
+        "{header[0]}" TEXT,
+        "{header[1]}" TEXT,
+        "{header[2]}" TEXT,
+        "{header[3]}" TEXT,
+        "{header[4]}" TEXT,
+        "{header[5]}" TEXT,
+        "{header[6]}" TEXT,
+        "{header[7]}" TEXT,
+        "{header[8]}" TEXT,
+        "{header[9]}" TEXT,
+        "{header[10]}" TEXT,
+        "{header[11]}" TEXT,
+        "{header[12]}" TEXT,
+        "{header[13]}" TEXT,
+        "{header[14]}" TEXT,
+        "{header[15]}" TEXT,
+        "{header[16]}" TEXT,
+        "{header[17]}" TEXT,
+        "{header[18]}" TEXT,
+        "{header[19]}" TEXT,
+        "{header[20]}" TEXT,
+        "{header[21]}" TEXT
+        )'''
+
+cs.execute(query)
+conn.commit()
+
+def column_select(HEADER, a, b):
+    text=""
+    for i in range(a, b):
+        text += '"'+HEADER[i]+'",'
+    text = text[:-1]
+    return text
+
+def insert_value(CONTENTS, a, b, k):
+    text = ""
+    for i in range(a,b):
+        text += '"'+str(CONTENTS[k][i])+'",'
+    text = text[:-1]
+    return text
+
+data_sql = ''' select count(*) from FREE_BOOKS '''
+cs.execute(data_sql)
+data_count = cs.fetchone()
+
+if args.init == True or data_count[0] <= 400:
+    COLUMN = column_select(header, 0, len(header))
+
+    for i in range(len(contents)):
+        VALUES = insert_value(contents, 0, len(header), i)
+        query = f'INSERT into FREE_BOOKS ({COLUMN}) values ({VALUES})'
+        cs.execute(query)
+    conn.commit()
+    print("데이터 입력 완료")
+
+R = True
+while R:
+    R = False
+    print("도서의 제목 또는 분류에 포함된 검색할 단어를 입력하세요.")
+    words = str(input("검색어: "))
+    query_select = f'''
+    select "Book Title", "Author", "Edition", "OpenURL", "Print ISBN", "English Package Name" from FREE_BOOKS 
+    where "Book Title" like "%{words}%" or "Subject Classification" like "%{words}%" ;
+    '''
+    results = cs.execute(query_select)
+
+    print("\n")
+    print("#다운 받을 항목을 선택해주세요")
+    num = 1
+    books = []
+    for i in results:
+        print(f"{num}. "+i[0])
+        books.append(i)
+        num += 1
+    print(f"{num}. 검색결과 모두 다운로드")
+
+    try:
+        book_num = int(input("번호선택: "))
+        print("\n")
+        print("# 다운로드 받을 파일 형식을 선택해주세요")
+        print("1. pdf")
+        print("2. epub")
+        print("3. 모두")
+        filetype_num = int(input("번호선택: "))
+    except Exception as e:
+        print("\n")
+        print("*잘못 누르셨습니다.(%s)" % e)
+        R = True
+        continue
+
+    if book_num > num or book_num < 0 or filetype_num < 0 or filetype_num > 3:
+        print("\n")
+        print("잘못 입력하였습니다. 종료")
+        R = True
+        continue
+
+    patches = []
+    if filetype_num == 1:
+        patches.append({'url': '/content/pdf/', 'ext': '.pdf'})
+    elif filetype_num == 2:
+        patches.append({'url':'/download/epub/', 'ext':'.epub'})
+    else:
+        patches.append({'url': '/content/pdf/', 'ext': '.pdf'})
+        patches.append({'url':'/download/epub/', 'ext':'.epub'})
+
+book = []
+if book_num < num:
+    book.append(books[book_num])
+    download_books(book, folder, patches)
+else:
+    download_books(books, folder, patches)
